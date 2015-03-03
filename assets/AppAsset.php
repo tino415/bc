5faf328e85247df13eb4223dd52426655fff8eee
tino415 <cernakmartin3@gmail.com>
2015-03-03 01:14:20 +0100

    Removed google pain

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -18,8 +18,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/bootstrap.min.css',
-        'css/styles.css',
+        'css/site.css',
     ];
     public $js = [
     ];
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -13,145 +13,58 @@ AppAsset::register($this);
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
-    <head>
-        <meta http-equiv="content-type"
-            content="text/html; charset=<?= Yii::$app->charset ?>">
-        <meta charset="<?= Yii::$app->charset ?>">
-        <meta name="viewport"
-            content="width=device-width, initial-scale=1, maximum-scale=1">
-        <title><?= Html::encode($this->title) ?></title>
-        <?= Html::csrfMetaTags() ?>
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <!--[if lt IE 9]>
-            <script
-                src="http://html5shim.googlecode.com/svn/trunk/html5.js">
-            </script>
-        <![endif]-->
-        <link href="css/styles.css" rel="stylesheet">
-    </head>
-    <body>
+<head>
+    <meta charset="<?= Yii::$app->charset ?>"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
 
 <?php $this->beginBody() ?>
-<nav class="navbar navbar-fixed-top header">
-    <div class="col-md-12">
-        <div class="navbar-header">
-          
-          <a href="#" class="navbar-brand">Bootstrap</a>
-          <button type="button"
-            class="navbar-toggle" data-toggle="collapse"
-            data-target="#navbar-collapse1">
-          <i class="glyphicon glyphicon-search"></i>
-          </button>
-      
+    <div class="wrap">
+        <?php
+            NavBar::begin([
+                'brandLabel' => 'My Company',
+                'brandUrl' => Yii::$app->homeUrl,
+                'options' => [
+                    'class' => 'navbar-inverse navbar-fixed-top',
+                ],
+            ]);
+            echo Nav::widget([
+                'options' => ['class' => 'navbar-nav navbar-right'],
+                'items' => [
+                    ['label' => 'Main', 'url' => ['/site/main']],
+                    ['label' => 'Home', 'url' => ['/site/index']],
+                    ['label' => 'About', 'url' => ['/site/about']],
+                    ['label' => 'Contact', 'url' => ['/site/contact']],
+                    Yii::$app->user->isGuest ?
+                        ['label' => 'Login', 'url' => ['/site/login']] :
+                        ['label' => 'Logout (' . Yii::$app->user->identity->username . ')',
+                            'url' => ['/site/logout'],
+                            'linkOptions' => ['data-method' => 'post']],
+                ],
+            ]);
+            NavBar::end();
+        ?>
+
+        <div class="container">
+            <?= Breadcrumbs::widget([
+                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+            ]) ?>
+            <?= $content ?>
         </div>
-        <div class="collapse navbar-collapse" id="navbar-collapse1">
-          <form class="navbar-form pull-left">
-              <div class="input-group" style="max-width:470px;">
-                <input type="text" class="form-control"
-                    placeholder="Search" name="srch-term" id="srch-term">
-                <div class="input-group-btn">
-                    <button class="btn btn-default btn-primary" type="submit">
-                        <i class="glyphicon glyphicon-search"></i>
-                    </button>
-                </div>
-              </div>
-          </form>
-          <ul class="nav navbar-nav navbar-right">
-             <li><a href="http://www.bootply.com" target="_ext">Bootply+</a></li>
-             <li>
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                    <i class="glyphicon glyphicon-bell"></i></a>
-                <ul class="dropdown-menu">
-                    <li><a href="#">
-                        <span class="badge pull-right">40</span>Link</a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <span class="badge pull-right">2</span>Link
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#"><span class="badge pull-right">0
-                            </span>Link</a></li>
-                    <li><a href="#">
-                        <span class="label label-info pull-right">1</span>Link
-                        </a>
-                    </li>
-                    <li><a href="#">
-                    <span class="badge pull-right">13</span>Link</a></li>
-                </ul>
-             </li>
-             <li><a href="#" id="btnToggle"><i class="glyphicon glyphicon-th-large"></i></a></li>
-             <li><a href="#"><i class="glyphicon glyphicon-user"></i></a></li>
-           </ul>
-        </div>  
-     </div> 
-</nav>
-<div class="navbar navbar-default" id="subnav">
-    <div class="col-md-12">
-        <div class="navbar-header">
-          
-          <a href="#" style="margin-left:15px;" class="navbar-btn btn btn-default btn-plus dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-home" style="color:#dd1111;"></i> Home <small><i class="glyphicon glyphicon-chevron-down"></i></small></a>
-          <ul class="nav dropdown-menu">
-              <li><a href="#"><i class="glyphicon glyphicon-user" style="color:#1111dd;"></i> Profile</a></li>
-              <li><a href="#"><i class="glyphicon glyphicon-dashboard" style="color:#0000aa;"></i> Dashboard</a></li>
-              <li><a href="#"><i class="glyphicon glyphicon-inbox" style="color:#11dd11;"></i> Pages</a></li>
-              <li class="nav-divider"></li>
-              <li><a href="#"><i class="glyphicon glyphicon-cog" style="color:#dd1111;"></i> Settings</a></li>
-              <li><a href="#"><i class="glyphicon glyphicon-plus"></i> More..</a></li>
-          </ul>
-          
-          
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse2">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          </button>
-      
+    </div>
+
+    <footer class="footer">
+        <div class="container">
+            <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+            <p class="pull-right"><?= Yii::powered() ?></p>
         </div>
-        <div class="collapse navbar-collapse" id="navbar-collapse2">
-          <ul class="nav navbar-nav navbar-right">
-             <li class="active"><a href="#">Posts</a></li>
-             <li><a href="#loginModal" role="button" data-toggle="modal">Login</a></li>
-             <li><a href="#aboutModal" role="button" data-toggle="modal">About</a></li>
-           </ul>
-        </div>  
-     </div> 
-</div>
+    </footer>
 
-<!--main-->
-<div class="container" id="main">
-    <?= $content ?>
-</div>
-
-
-<!--about modal-->
-<div id="aboutModal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
-  <div class="modal-dialog">
-  <div class="modal-content">
-      <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-          <h2 class="text-center">About</h2>
-      </div>
-      <div class="modal-body">
-          <div class="col-md-12 text-center">
-            <a href="http://www.bootply.com/DwnjTNuvVt">This Bootstrap Template</a><br>was made with <i class="glyphicon glyphicon-heart"></i> by <a href="http://bootply.com/templates">Bootply</a>
-            <br><br>
-            <a href="https://github.com/iatek/bootstrap-google-plus">GitHub Fork</a>
-          </div>
-      </div>
-      <div class="modal-footer">
-          <button class="btn" data-dismiss="modal" aria-hidden="true">OK</button>
-      </div>
-  </div>
-  </div>
-</div>
-    <!-- script references -->
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js/scripts.js"></script>
 <?php $this->endBody() ?>
-    </body>
+</body>
 </html>
 <?php $this->endPage() ?>
