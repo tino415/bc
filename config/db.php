11b6f63e3a947d929b0611cddee288b148296db2
tino415 <cernakmartin3@gmail.com>
2015-03-03 01:02:05 +0100

    Unkillable

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=bc',
+    'username' => 'bc',
+    'password' => 'bc',
+    'charset' => 'utf8',
+];
