a5357699adbea8f070ad73c19f4b35335a57c6ac
tino415 <cernakmartin3@gmail.com>
2015-03-18 12:20:47 +0100

    Removed breadcrumbs from main templates

--- views/layouts/fluid.php
+++ views/layouts/fluid.php
@@ -3,11 +3,6 @@ use yii\widgets\Breadcrumbs;
 ?>
 
 <?php $this->beginContent('@app/views/layouts/mainlay.php'); ?>
-        <div class="container">
-            <?= Breadcrumbs::widget([
-                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-            ]) ?>
-        </div>
-
-        <div class="container-fluid"><?= $content ?></div>
+    <div class="container"></div>
+    <div class="container-fluid"><?= $content ?></div>
 <?php $this->endContent();
