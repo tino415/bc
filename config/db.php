860ae57d2ddf111435ae84650846aa51f47d09e2
tino415 <cernakmartin3@gmail.com>
2015-05-01 11:37:54 +0200

    Error log!

--- config/db.php
+++ config/db.php
@@ -1,10 +1,12 @@
 <?php
 
+error_log("Database setup");
 $dbopts = parse_url(getenv('DATABASE_URL'));
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host='.$dbopts['host'].';port:'.$dbopts['port'].';dbname='.ltrim($dbopts['path'], '/'),
+    'dsn' => 'pgsql:host='.$dbopts['host'].';'.
+        'port:'.$dbopts['port'].';dbname='.ltrim($dbopts['path'], '/'),
     'username' => $dbopts['user'],
     'password' => $dbopts['pass'],
     'charset' => 'utf8',
