cbc332163aa8a91f56d3b9861f9ca76330c44400
tino415 <cernakmartin3@gmail.com>
2015-05-01 12:07:27 +0200

    Master

--- controllers/DocumentController.php
+++ controllers/DocumentController.php
@@ -27,7 +27,6 @@ class DocumentController extends Controller {
     }
 
     public function actionIndex($query = null) {
-        exit("WHT");
         return $this->render('index',[
             'phrase' => $query,
             'results' => (is_null($query)) ?
