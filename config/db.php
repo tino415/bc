1841c0bf3253e21b6b83ee71c62f62816b5fbfd5
tino415 <cernakmartin3@gmail.com>
2015-05-03 15:22:20 +0200

    Heroku config loaded

--- config/db.php
+++ config/db.php
@@ -1,35 +1,13 @@
 <?php
 
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:'.
-            'host=localhost;'.
-            'port=5432;'.
-            'dbname=bc',
-    'username' => 'bc',
-    'password' => 'bc',
-    'charset' => 'utf8',
-    'schemaMap' => [
-        'pgsql' => [
-            'class' => 'yii\db\pgsql\Schema',
-            'defaultSchema' => 'public',
-        ]
-    ]
-];
-
-// Heroku
-//$dbopts = parse_url(getenv('DATABASE_URL'));
-//extract($dbopts);
-//$dbname = ltrim($path,'/');
-//
 //return [
 //  'class' => 'yii\db\Connection',
 //  'dsn' => 'pgsql:'.
-//          "host=$host;".
-//          "port=$port;".
-//          "dbname=$dbname",
-//  'username' => $user,
-//  'password' => $pass,
+//          'host=localhost;'.
+//          'port=5432;'.
+//          'dbname=bc',
+//  'username' => 'bc',
+//  'password' => 'bc',
 //  'charset' => 'utf8',
 //  'schemaMap' => [
 //      'pgsql' => [
@@ -38,3 +16,25 @@ return [
 //      ]
 //  ]
 //];
+
+// Heroku
+$dbopts = parse_url(getenv('DATABASE_URL'));
+extract($dbopts);
+$dbname = ltrim($path,'/');
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:'.
+            "host=$host;".
+            "port=$port;".
+            "dbname=$dbname",
+    'username' => $user,
+    'password' => $pass,
+    'charset' => 'utf8',
+    'schemaMap' => [
+        'pgsql' => [
+            'class' => 'yii\db\pgsql\Schema',
+            'defaultSchema' => 'public',
+        ]
+    ]
+];
