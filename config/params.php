fc4f880784473e4c8aec9c0ba01771d11d323e2f
tino415 <cernakmartin3@gmail.com>
2023-05-08 11:11:32 +0200

    Doc clean rm lfm

--- config/params.php
+++ config/params.php
@@ -6,5 +6,7 @@ return [
     'long_term_groups' => '5',
     'min_tag_length' => '2',
     'search_session_timeout' => 30 * 60, // min * sec
+    'last_fm_api_key' => '0bec43a6e9d33b47df0f02df11e84960',
+    'last_fm_secret' => 'is ae2293cd5e165bd33a7d47a6ab811570',
     'stopwords' => require(__DIR__ . '/stopwords.php'),
 ];
